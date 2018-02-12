7d4ed924a8b8b27801ba2cc188d4f7ebbeb6e138
seemarao <seema.rao@prueba.io>
2018-02-12 12:42:33 +0530

    Initial commit

---
+++ Leave_appz/Leave_appz/Leave_appzPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Leave_appz
+{
+    public partial class Leave_appzPage : ContentPage
+    {
+        public Leave_appzPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
