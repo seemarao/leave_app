a86b6f5e61ce7b97eea24c7f59ff116023724702
Pratik Kamat <pratikkamat@Users-MacBook-Pro.local>
2018-02-20 17:20:21 +0530

    Sick Leave Page

--- Leave_appz/Leave_appz/App.xaml.cs
+++ Leave_appz/Leave_appz/App.xaml.cs
@@ -8,7 +8,7 @@ namespace Leave_appz
         {
             InitializeComponent();
 
-            MainPage = new LeaveInfo();
+            MainPage = new sickLeaveRequestPage();
         }
 
         protected override void OnStart()
---
+++ Leave_appz/Leave_appz/sickLeaveRequestPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Leave_appz
+{
+    public partial class sickLeaveRequestPage : ContentPage
+    {
+        public sickLeaveRequestPage()
+        {
+            InitializeComponent();
+            backgroundImage.Source = ImageSource.FromResource("Leave_appz.Assets.background.png");
+
+        }
+    }
+}
